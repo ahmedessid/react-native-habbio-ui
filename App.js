f7a643a503327383ced85a23579ba123e2df5cdb
ahmed.essidmu <ahmed.essid@apeiron-tech.com>
2022-03-19 10:13:30 +0100

    add app screens with styles styling

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import IntroScreen from './lib/screens/introduction/IntroScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import IntroScreen from "./lib/screens/introduction/IntroScreen";
+import Home from "./lib/screens/home/Home";
+import AddHabit from "./lib/screens/add_habit/AddHabit";
+
+const Stack = createNativeStackNavigator();
+
+const screenOptions = {
+  headerShown: false,
+  contentStyle: {
+    backgroundColor: "#FFFFFF",
+  },
+};
 
 export default function App() {
-  return <IntroScreen/>;
+  return (
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator screenOptions={screenOptions}>
+          <Stack.Screen name="Intro" component={IntroScreen} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="AddHabit" component={AddHabit} />
+        </Stack.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
+  );
 }
-
